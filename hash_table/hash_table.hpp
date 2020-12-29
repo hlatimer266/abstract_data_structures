b36bd03007043826120191be7015d3bb243537ce
hlatimer266 <hlatimer266@gmail.com>
2020-12-29 03:12:32 -0800

    add hash table

--- hash_table/hash_table.hpp
+++ hash_table/hash_table.hpp
@@ -1,21 +1,19 @@
-#ifndef HASH_MAP_HPP
-#define HASH_MAP_HPP
-
-#include <string>
+#ifndef HASH_TABLE_HPP
+#define HASH_TABLE_HPP
+#include <list> 
 
 using namespace std;
 
-struct hash_link {
-	string key;
-	int data;
-	struct hash_link* next;
-}
-
-class hash_map{
+class hash_table{
 	private:
+		int size;
+		list<int> *table;
 	public:
-		hash_map();
-		~hash_map();
-		
-
-} 
+		hash_table(int size_map);
+		~hash_table();
+		void insert_item(int x);
+		void delete_item(int key);
+		int hash_key(int x);
+		void display_all();
+}; 
+#endif
